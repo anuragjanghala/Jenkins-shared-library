9d8f52bdba3a57854e6f54919e7c0a9b055c8fba
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-04-24 20:45:33 +0100

    added cred

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-pri-repo-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t ajanghala/my-private-repo:3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push ajanghala/my-private-repo:tagname'
     }
 }
